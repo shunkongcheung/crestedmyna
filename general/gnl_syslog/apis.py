824ac630753e5a558ba06cb64663f7ad785f2ca3
shun.cheung <shun.cheung@radicasys.com>
2019-09-05 00:31:27 +0900

    backend: gnl_syslog/apis IsAdminUser

--- general/gnl_syslog/apis.py
+++ general/gnl_syslog/apis.py
@@ -1,9 +1,9 @@
 from base.apis import MyListAPIView
-
+from rest_framework.permissions import IsAdminUser
 
 from .models import SystemLog
 
 
 class SystemLogListAPIView(MyListAPIView):
-    # permission_classes = []
+    permission_classes = [IsAdminUser, ]
     model = SystemLog
