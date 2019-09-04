9960ac4eb69bdef9bcfa097ef9c952321dbf21df
shun.cheung <shun.cheung@radicasys.com>
2019-09-05 00:28:08 +0900

    backend: list_apis get_serializer_class

--- base/apis/list_apis.py
+++ base/apis/list_apis.py
@@ -1,7 +1,35 @@
-from .base_apis import MyBaseAPIView
-from base.serializers.list_serializer import MyListViewSerializer
+from base.serializers.list_serializer import MyListViewBaseSerializer
+from django.utils import timezone
 from rest_framework.generics import ListAPIView
+from rest_framework.serializers import (
+    DateTimeField,
+    ModelSerializer,
+    SlugRelatedField,
+)
+
+from .base_apis import MyBaseAPIView
+
+
+class LocalTime(DateTimeField):
+    def to_representation(self, value):
+        return timezone.localtime(value)
+
+
+class MyListViewBaseSerializer(ModelSerializer):
+    created_by = SlugRelatedField(slug_field='username', read_only=True)
+    created_at = LocalTime(read_only=True)
+    modified_at = LocalTime(read_only=True)
 
 
 class MyListAPIView(ListAPIView, MyBaseAPIView):
-    serializer_class = MyListViewSerializer
+    fields = ['name', ]
+
+    def get_serializer_class(self, *args, **kwargs):
+        model_fields = ['id', ] + self.fields
+
+        class MyListViewSerializer(MyListViewBaseSerializer):
+            class Meta:
+                model = self.model
+                fields = model_fields
+
+        return MyListViewSerializer
