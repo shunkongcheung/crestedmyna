c8d409ffd2db1675937e7d2e9ddb6cedb7feeb7e
shun.cheung <shun.cheung@radicasys.com>
2019-09-05 00:42:35 +0900

    backend: base_apis remove console

--- base/apis/base_apis.py
+++ base/apis/base_apis.py
@@ -6,7 +6,6 @@ class MyBaseAPIView(GenericAPIView):
     permissions_classes = [IsAuthenticated, ]
 
     def get_queryset(self):
-        print('hey here....', self.request.user)
         return self.model.objects.filter(created_by=self.request.user,
                                          enable=True
                                          )
