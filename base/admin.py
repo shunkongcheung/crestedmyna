47bedd5b3d1f6d49977e5260d168fed084d6257e
shun.cheung <shun.cheung@radicasys.com>
2019-09-08 18:27:11 +0900

    backend: base/admin - handle list_filter

--- base/admin.py
+++ base/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 
 class MyBaseAdmin(admin.ModelAdmin):
     list_display = []
-    list_filter = ['enable', 'created_by', ]
+    list_filter = []
     search_fields = []
 
     def insert_list_display(self, item, is_prepend=False):
@@ -19,14 +19,25 @@ class MyBaseAdmin(admin.ModelAdmin):
         if item not in self.search_fields:
             self.search_fields += [item, ]
 
+    def insert_list_filter(self, item):
+        if item not in self.list_filter:
+            self.list_filter += [item, ]
+
     def __init__(self, *args, **kwargs):
+        # list display ----------------------------------------------------
         self.insert_list_display('name', is_prepend=True)
 
         insert_items = ['enable', 'created_by', 'created_at', 'modified_at', ]
         for item in insert_items:
             self.insert_list_display(item)
 
+        # search fields ---------------------------------------------------
         self.insert_search_fields('name')
+
+        # list filter -----------------------------------------------------
+        insert_filters = ['enable', 'created_by', ]
+        for item in insert_filters:
+            self.insert_list_filter(item)
 
         ret = super(MyBaseAdmin, self).__init__(*args, **kwargs)
         return ret
