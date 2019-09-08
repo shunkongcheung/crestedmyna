b85f36a71e547b1621ceff751a2b2b9b8a9f054b
shun.cheung <shun.cheung@radicasys.com>
2019-09-08 18:08:28 +0900

    backend: base_apis handle read_only_fields

--- base/apis/base_apis.py
+++ base/apis/base_apis.py
@@ -21,17 +21,20 @@ class MyBaseSerializer(ModelSerializer):
 
 class MyBaseAPIView(GenericAPIView):
     fields = ['name', ]
+    read_only_fields = []
     permissions_classes = [IsAuthenticated, ]
 
     def get_serializer_class(self, *args, **kwargs):
         prepend_fields = ['id', ]
         propend_fields = ['created_by', 'created_at', 'modified_at', ]
-        model_fields = prepend_fields + self.fields + propend_fields
+        model_fields = prepend_fields + self.fields + \
+            self.read_only_fields + propend_fields
 
         class MyBaseViewSerializer(MyBaseSerializer):
             class Meta:
                 model = self.model
                 fields = model_fields
+                read_only_fields = self.read_only_fields
 
         return MyBaseViewSerializer
 
