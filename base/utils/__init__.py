49a1e5eb6771c08344e88307a6ff0068071ea759
shun.cheung <shun.cheung@radicasys.com>
2019-09-08 11:29:10 +0900

    backend: get_admin_user

---
+++ base/utils/__init__.py
@@ -0,0 +1,1 @@
+from .get_admin_user import get_admin_user
---
+++ base/utils/get_admin_user.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.models import User
+
+
+def get_admin_user():
+    return User.objects.get(username='shun.cheung')
