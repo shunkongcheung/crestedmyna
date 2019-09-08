ed23d6d86f704042bce64acb53a4e37d56af784e
shun.cheung <shun.cheung@radicasys.com>
2019-09-08 21:45:30 +0900

    backend: get_stock_master_share_count_by_type

---
+++ stock/stk_tx/utils/get_stock_master_share_count_by_type.py
@@ -0,0 +1,12 @@
+from django.db.models import Sum
+
+from .get_stock_tx_basic_filter import get_stock_tx_basic_filter
+
+
+def get_stock_master_share_count_by_type(stock_master,
+                                         tx_type,
+                                         created_after,
+                                         created_before):
+    kwargs = get_stock_tx_basic_filter(tx_type, created_after, created_before)
+    return stock_master.stock_txs.filter(**kwargs)\
+        .aggregate(Sum('share_count'))['share_count__sum'] or 0
