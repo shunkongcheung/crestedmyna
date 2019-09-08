2e152a4c253982c812799da952f710a88072b7a2
shun.cheung <shun.cheung@radicasys.com>
2019-09-08 21:47:15 +0900

    backend: stock/stk_tx/utils/get_stock_tx_net_value.py

---
+++ stock/stk_tx/utils/get_stock_tx_net_value.py
@@ -0,0 +1,8 @@
+from stock.models import StockTx
+
+
+def get_stock_tx_net_value(gross_value, trade_cost, tx_type):
+    if tx_type == StockTx.TX_BUY[0]:
+        return gross_value + trade_cost
+    else:
+        return gross_value - trade_cost
