785de95e4e065cc28fe272322ce3643dc2956dac
shun.cheung <shun.cheung@radicasys.com>
2019-09-10 16:09:20 +0800

    frontend: FourOFour add animation

--- frontend/src/base/Utils/FourOFour.tsx
+++ frontend/src/base/Utils/FourOFour.tsx
@@ -1,25 +1,54 @@
-import React, { memo } from "react";
+import React, { memo, useCallback, useMemo } from "react";
 import { Link } from "react-router-dom";
+import { animated, useTrail } from "react-spring";
 
-import classes from "./FourOFour.module.css";
+import Layout from "../Layout/Layout";
+
+import classes from "./FourOFour.module.scss";
+
+const items = [4, 0, 4];
+const config = { mass: 5, tension: 2000, friction: 200 };
 
 function FourOFour() {
+  const trail = useTrail(items.length, {
+    config,
+    opacity: 1,
+    x: 0,
+    from: { opacity: 0, x: 20 }
+  });
+  const transform = useCallback(
+    x => x.interpolate((x: number) => `translate3d(0,${x}px,0)`),
+    []
+  );
+
+  const rendered404 = useMemo(
+    () => {
+      return trail.map(({ x, ...rest }, index) => {
+        const key = items[index] + "-" + index;
+        const className = index % 2 === 0 ? undefined : classes.bannerZero;
+        return (
+          <animated.div key={key} style={{ ...rest, transform: transform(x) }}>
+            <animated.div className={className}>{items[index]}</animated.div>
+          </animated.div>
+        );
+      });
+    },
+    [trail, transform]
+  );
   return (
-    <div className={classes.containerOuter}>
-      <div className={classes.containerInner}>
-        <div className={classes.bannerContainer}>
-          <h1 className={classes.banner404}>
-            <span className={classes.bannerFour}>4</span>
-            <span className={classes.bannerZero}>0</span>
-            <span className={classes.bannerFour}>4</span>
-          </h1>
+    <Layout>
+      <div className={classes.containerOuter}>
+        <div className={classes.containerInner}>
+          <div className={classes.bannerContainer}>
+            <h1 className={classes.banner404}>{rendered404}</h1>
+          </div>
+          <h2>The page you requested could not found</h2>
+          <Link className={classes.homeAnchor} to="/">
+            Home
+          </Link>
         </div>
-        <h2>The page you requested could not found</h2>
-        <Link className={classes.homeAnchor} to="/">
-          Home
-        </Link>
       </div>
-    </div>
+    </Layout>
   );
 }
 
