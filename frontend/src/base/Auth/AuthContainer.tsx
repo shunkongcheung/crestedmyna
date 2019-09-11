d85d9ba4ce834f2378a4836e7a6b8e47af6c8142
shun.cheung <shun.cheung@radicasys.com>
2019-09-11 23:17:28 +0800

    frontend: AuthContainer add login url

--- frontend/src/base/Auth/AuthContainer.tsx
+++ frontend/src/base/Auth/AuthContainer.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, RouteComponentProps, Switch } from "react-router-dom";
 
 import LoginView from "./LoginView";
+import FourOFour from "../Utils/FourOFour"
 
 function AuthContainer({ match }: RouteComponentProps) {
   return (
     <Switch>
-      <Route path={`${match.url}/login/`} exact component={LoginView} />
+			<Route path={`${match.url}/login/`} exact component={LoginView} />
+			<Route path="*" component={FourOFour} />
     </Switch>
   );
 }
