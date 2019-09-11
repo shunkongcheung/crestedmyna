c187d9a18040ee3e4162cf983007339cd29503b2
shun.cheung <shun.cheung@radicasys.com>
2019-09-12 00:43:01 +0800

    frontend: Header - switch auth ctrls

--- frontend/src/base/Layout/Header.tsx
+++ frontend/src/base/Layout/Header.tsx
@@ -1,6 +1,9 @@
-import React, { memo } from "react";
-import { Link } from "react-router-dom"
+import React, { memo, useCallback, useContext, useMemo } from "react";
+import { animated, useSpring } from "react-spring";
+import { Link, RouteComponentProps, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
+
+import AuthContext from "../Contexts/AuthContext";
 
 import classes from "./Header.module.scss";
 
@@ -8,17 +11,55 @@ interface IHeaderProps {
   headerHeight: number;
 }
 
-function Header({ headerHeight }: IHeaderProps) {
+function Header({ headerHeight, history }: IHeaderProps & RouteComponentProps) {
+  const { expireAt, handleTokenChange } = useContext(AuthContext);
+  const isLogined = useMemo(() => new Date(expireAt) >= new Date(), [expireAt]);
+  const inStyle = useSpring({
+    opacity: 1,
+    from: { opacity: 0 }
+  });
+
+  const handleLogout = useCallback(
+    () => {
+      handleTokenChange("");
+      history.push("/uam/login/");
+    },
+    [handleTokenChange, history]
+  );
+
+  const renderedAuthCtrl = useMemo(
+    () => (
+      <span className={classes.rightCtrl} onClick={handleLogout}>
+        SIGN OUT
+      </span>
+    ),
+    [inStyle, handleLogout]
+  );
+  const renderedUnAuthCtrl = useMemo(
+    () => (
+      <>
+        <Link className={classes.rightCtrl} to="/uam/login/">
+          SIGN IN
+        </Link>
+        <Link className={classes.registerCtrl} to="/uam/register/">
+          REGISTER
+        </Link>
+      </>
+    ),
+    []
+  );
   return (
-    <div className={classes.container} style={{ height: headerHeight }}>
+    <animated.div
+      className={classes.container}
+      style={{ height: headerHeight, ...inStyle }}
+    >
       <div className={classes.siteName}>DAILY</div>
-			<Link className={classes.rightCtrl} to="/uam/login/">SIGN IN</Link>
-			<Link className={classes.registerCtrl} to="/uam/register/">REGISTER</Link>
-    </div>
+      {isLogined ? renderedAuthCtrl : renderedUnAuthCtrl}
+    </animated.div>
   );
 }
 
 Header.propTypes = {
   headerHeight: PropTypes.number.isRequired
 };
-export default memo(Header);
+export default memo(withRouter(Header));
