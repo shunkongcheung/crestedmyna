7ac86e01f0d8b6bad8fd0841a46d37a9aa0c8aab
shun.cheung <shun.cheung@radicasys.com>
2019-09-12 10:01:54 +0800

    frontend: Layout center logic

--- frontend/src/base/Layout/Layout.tsx
+++ frontend/src/base/Layout/Layout.tsx
@@ -1,8 +1,10 @@
-import React, { ReactNode, memo, useState } from "react";
+import React, { ReactNode, memo, useMemo, useState } from "react";
 import PropTypes from "prop-types";
 
 import Header from "./Header";
 import NavDrawer from "./NavDrawer";
+
+import classes from "./Layout.module.scss";
 
 interface ILayoutProps {
   children: ReactNode;
@@ -11,23 +13,31 @@ interface ILayoutProps {
 function Layout({ children, unAuth = false }: ILayoutProps) {
   const headerHeight = 70;
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  return (
-    <div id="outer-container">
-      {unAuth ? (
-        undefined
-      ) : (
+
+  const renderedDrawer = useMemo(
+    () => {
+      if (unAuth) return <></>;
+      return (
         <NavDrawer
           headerHeight={headerHeight}
           isDrawerOpen={isDrawerOpen}
           outerContainerId="outer-container"
           pageWrapId="page-wrap"
-setIsDrawerOpen={setIsDrawerOpen}
+          setIsDrawerOpen={setIsDrawerOpen}
         />
-      )}
+      );
+    },
+    [headerHeight, isDrawerOpen, setIsDrawerOpen]
+  );
+
+  return (
+    <div id="outer-container">
+      {renderedDrawer}
       <main id="page-wrap">
         <Header headerHeight={headerHeight} setIsDrawerOpen={setIsDrawerOpen} />
-        <div style={{ marginTop: headerHeight }} />
-        {children}
+        <div className={classes.container}>
+          <div className={classes.content}>{children}</div>
+        </div>
       </main>
     </div>
   );
