d188e766a13c86ebe6c1a390d2fabee7cadcdc00
shun.cheung <shun.cheung@radicasys.com>
2019-09-12 10:03:41 +0800

    frontend: HomeContainer done nothing

--- frontend/src/home/HomeContainer.tsx
+++ frontend/src/home/HomeContainer.tsx
@@ -1,23 +1,13 @@
 import React, { memo } from "react";
-/* import PropTypes from "prop-types"; */
 
 import Layout from "../base/Layout/Layout";
 
-interface IHomeContainerProps {}
-
-/* function HomeContainer({  }: IHomeContainerProps) { */
 function HomeContainer() {
   return (
     <Layout>
       <div
         style={{
-          alignItems: "center",
-          color: "white",
-          display: "flex",
-          flexDirection: "column",
-          fontSize: "calc(10px + 2vmin)",
-          justifyContent: "center",
-          minHeight: "100vh"
+          fontSize: "calc(10px + 2vmin)"
         }}
       >
         Hie i am home
@@ -26,5 +16,4 @@ function HomeContainer() {
   );
 }
 
-HomeContainer.propTypes = {};
 export default memo(HomeContainer);
