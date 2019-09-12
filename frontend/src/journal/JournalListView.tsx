cb2b66ad6c1c570b55dce7c5328ca33ed24e7d15
shun.cheung <shun.cheung@radicasys.com>
2019-09-12 22:06:00 +0800

    frontend: JournalListView add Calendar

--- frontend/src/journal/JournalListView.tsx
+++ frontend/src/journal/JournalListView.tsx
@@ -1,10 +1,33 @@
 import React, { memo } from "react";
-import { RouteComponentProps } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import Layout from "../base/Layout/Layout";
+import Calendar from "../base/Utils/Calendar";
 
-function JournalListView({ history, location }: RouteComponentProps) {
-  return <Layout>journal list view</Layout>;
+interface IEvent {
+  id: number;
+  title: string;
+  start: Date;
+  end: Date;
 }
+interface IJournalListViewProps {
+  events: Array<IEvent>;
+  handleRangeChange: (a: Date, b: Date) => any;
+}
+function JournalListView({ events, handleRangeChange }: IJournalListViewProps) {
+  return (
+      <div style={{ height: "70vh" }}>
+        <Calendar
+          events={events}
+          handleEventClick={id => console.log(id)}
+          handleRangeChange={handleRangeChange}
+        />
+      </div>
+  );
+}
+
+JournalListView.propTypes = {
+  events: PropTypes.array.isRequired,
+  handleRangeChange: PropTypes.func.isRequired
+};
 
 export default memo(JournalListView);
