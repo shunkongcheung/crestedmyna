87ebfc077682f9c606fc6f3448acaae7c3adf7ab
shun.cheung <shun.cheung@radicasys.com>
2019-09-15 10:21:28 +0800

    frontend: CenterArea withPadding

--- frontend/src/base/Utils/CenterArea.tsx
+++ frontend/src/base/Utils/CenterArea.tsx
@@ -5,17 +5,22 @@ import classes from "./CenterArea.module.scss";
 
 interface ICenterAreaProps {
   children: ReactNode;
+  withPadding?: boolean;
 }
 
-function CenterArea({ children }: ICenterAreaProps) {
+function CenterArea({ children, withPadding }: ICenterAreaProps) {
+  const className =
+    classes.containerInner +
+    (withPadding ? ` ${classes.containerWithPadding}` : "");
   return (
     <div className={classes.containerOuter}>
-      <div className={classes.containerInner}>{children}</div>
+      <div className={className}>{children}</div>
     </div>
   );
 }
 
 CenterArea.propTypes = {
-  children: PropTypes.element.isRequired
+  children: PropTypes.element.isRequired,
+  withPadding: PropTypes.bool
 };
 export default memo(CenterArea);
