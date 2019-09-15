7e10da0ab5be7eb600f433b9f23c12141cc8d655
shun.cheung <shun.cheung@radicasys.com>
2019-09-16 00:42:37 +0800

    Frontend: base/Fetches/useEditState.tsx

--- frontend/src/base/Fetches/index.tsx
+++ frontend/src/base/Fetches/index.tsx
@@ -1,4 +1,5 @@
 export { default as useDeleteState } from "./useDeleteState";
 export { default as useDetailState } from "./useDetailState";
 export { default as useEditState } from "./useEditState";
+export { default as useFetchFormDataState } from "./useFetchFormDataState";
 export { default as useListState } from "./useListState";
