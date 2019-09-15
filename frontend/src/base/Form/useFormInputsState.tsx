4d4c22a14ea415c2a02f74aa0ca4b1c5eb2a86ed
shun.cheung <shun.cheung@radicasys.com>
2019-09-15 23:42:18 +0800

    Frontend: useFormInputsState getByString

--- frontend/src/base/Form/useFormInputsState.tsx
+++ frontend/src/base/Form/useFormInputsState.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { FormikErrors, FormikProps } from "formik";
 
 function useFormState(
@@ -7,23 +7,44 @@ function useFormState(
 ) {
   const { errors, touched, submitCount, values } = formikProps;
 
+  const getByString = useCallback((o: any, s: string): any => {
+    s = s.replace(/\[(\w+)\]/g, ".$1"); // convert indexes to properties
+    s = s.replace(/^\./, ""); // strip a leading dot
+    let a = s.split(".");
+    try {
+      for (let i = 0, n = a.length; i < n; ++i) {
+        let k = a[i];
+        if (o !== undefined && o !== null && k in o) {
+          o = o[k];
+        } else return;
+      }
+    } catch (ex) {
+      return undefined;
+    }
+    return o;
+  }, []);
+
   const inputTouched = useMemo<boolean>(
     () => {
-      if (touched[name]) return true;
+      if (getByString(touched, name)) return true;
       return false;
     },
-    [name, touched]
+    [getByString, name, touched]
   );
 
   const inputError = useMemo<string | FormikErrors<any> | undefined>(
     () => {
       if (!inputTouched && submitCount === 0) return;
-      return errors[name];
+      return getByString(errors, name);
     },
-    [errors, inputTouched, name, submitCount]
+    [errors, getByString, inputTouched, name, submitCount]
   );
 
-  const inputValue = useMemo<any>(() => values[name], [name, values]);
+  const inputValue = useMemo<any>(() => getByString(values, name), [
+    getByString,
+    name,
+    values
+  ]);
 
   return { inputError, inputValue };
 }
