cd1aba439ad8506e605ded9aaf4184ca851cfa5e
shun.cheung <shun.cheung@radicasys.com>
2019-09-17 17:02:38 +0800

    backend: render react

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from rest_framework_jwt.views import (
     obtain_jwt_token,
@@ -30,4 +32,5 @@ urlpatterns = [
     # path('api/token/obtain/', obtain_jwt_token),
     # path('api/token/refresh/', refresh_jwt_token),
     path('admin/', admin.site.urls),
+    url(r'^', TemplateView.as_view(template_name='index.html')),
 ]
