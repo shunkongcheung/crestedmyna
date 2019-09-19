dcbb80a3ced6807ce807e4d920a8ba5bc32ea4be
shun.cheung <shun.cheung@radicasys.com>
2019-09-19 16:04:57 +0800

    frontend: Home add backe

---
+++ frontend/src/Home/HomeContainer.tsx
@@ -0,0 +1,8 @@
+import React, { memo } from "react";
+import HomeView from "./HomeView";
+
+function HomeContainer() {
+  return <HomeView />;
+}
+
+export default memo(HomeContainer);
---
+++ frontend/src/Home/LinkBanner.tsx
@@ -0,0 +1,35 @@
+import React, { memo } from "react";
+import { animated, useSpring } from "react-spring";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+import classes from "./LinkBanner.module.scss";
+
+interface ILinkBannerProps {
+  imageSrc: string;
+  linkTo: string;
+  name: string;
+}
+
+function LinkBanner({ name, imageSrc, linkTo }: ILinkBannerProps) {
+  const [props, set] = useSpring(() => ({ opacity: 0.4 }));
+  return (
+    <Link
+      className={classes.link}
+      to={linkTo}
+      onMouseEnter={() => set({ opacity: 0 })}
+      onMouseLeave={() => set({ opacity: 0.4 })}
+    >
+      <img className={classes.img} src={imageSrc} alt={linkTo} />
+      <animated.div className={classes.shadow} style={props} />
+      <h1 className={classes.name}>{name}</h1>
+    </Link>
+  );
+}
+
+LinkBanner.propTypes = {
+  imageSrc: PropTypes.string.isRequired,
+  linkTo: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
+};
+export default memo(LinkBanner);
