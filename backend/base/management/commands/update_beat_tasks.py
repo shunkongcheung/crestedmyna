32ad13033eaef4eaa621e8c191ccf21d05f00154
shun.cheung <shun.cheung@radicasys.com>
2019-09-19 14:53:31 +0800

    backend: update_beat_tasks use write_syslog

--- backend/base/management/commands/update_beat_tasks.py
+++ backend/base/management/commands/update_beat_tasks.py
@@ -1,7 +1,12 @@
+from base.utils import (
+    get_admin_user,
+    get_crontab_schedule_from_string,
+)
+
 from django_celery_beat.models import PeriodicTask
 from django.core.management.base import BaseCommand, CommandError
 
-from base.utils import get_crontab_schedule_from_string
+from general.gnl_syslog.utils import write_syslog
 from .utils import get_schedule_tasks
 
 
@@ -12,8 +17,8 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        print('update_beat_tasks: begin')
-
+        admin = get_admin_user()
+        log_name = 'update_beat_tasks'
         schedule_tasks = get_schedule_tasks()
         count = len(schedule_tasks)
 
@@ -25,6 +30,9 @@ class Command(BaseCommand):
                 name=name,
                 defaults={'task': task, 'crontab': crontab_schedule}
             )
-            print(f'update_beat_tasks: {idx}/{count:<5} {name}')
-
-        print('update_beat_tasks: finished.')
+            write_syslog(
+                log_name,
+                message=f'update_beat_tasks: {idx}/{count:<5} {name}',
+                created_by=admin,
+                is_print=True
+            )
