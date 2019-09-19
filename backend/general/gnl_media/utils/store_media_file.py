55964d77d26ce7ed0e492aece0e1386df75b2da3
shun.cheung <shun.cheung@radicasys.com>
2019-09-19 15:00:14 +0800

    backend: store_media_file get_file_path

--- backend/general/gnl_media/utils/store_media_file.py
+++ backend/general/gnl_media/utils/store_media_file.py
@@ -16,15 +16,21 @@ def get_unique_media_name(file_name, file_type):
 
 def get_media_file_full_path(file_name):
     if settings.IS_DEVELOPMENT:
-        return os.path.join(f'{BASE_DIR}', 'base', 'static', 'm', file_name)
-    return os.path.join(f'{BASE_DIR}{STATIC_URL}', 'm', file_name)
+        return os.path.join(f'{BASE_DIR}', 'base/static/m', file_name)
+    return os.path.join('/usr/local/casualapp/media', file_name)
+
+
+def get_media_file_path(unique_name):
+    host_name = get_lookup_value('HOST_NAME', 'GENERAL')
+    if settings.IS_DEVELOPMENT:
+        return f'{host_name}/static/m/{unique_name}'
+    return f'{host_name}/media/{unique_name}'
 
 
 def store_media_file(media_file, file_name, file_type):
     u_name = get_unique_media_name(file_name, file_type)
     media_path = get_media_file_full_path(u_name)
-    host_name = get_lookup_value('HOST_NAME', 'GENERAL')
     with open(media_path, 'wb') as file:
         file.write(media_file.read())
 
-    return f'{host_name}/static/m/{u_name}'
+    return get_media_file_path(u_name)
