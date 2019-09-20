7019b37ef6563ededa019db416aa0f47bf086710
shun.cheung <shun.cheung@radicasys.com>
2019-09-20 18:15:30 +0800

    frontend: npm i rc-tabs

--- frontend/src/external.d.ts
+++ frontend/src/external.d.ts
@@ -1,3 +1,7 @@
 declare module "draftjs-to-html";
 declare module "html-to-draftjs";
 declare module "react-page-scroller";
+
+declare module "rc-tabs";
+declare module "rc-tabs/lib/TabContent";
+declare module "rc-tabs/lib/ScrollableInkTabBar";
