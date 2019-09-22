5d569507acbd54287e8ef806098c30eae7302f8c
shun.cheung <shun.cheung@radicasys.com>
2019-09-22 12:36:43 +0800

    backend: update_stock_masters_market_price_and_value add log

--- backend/stock/stk_master/tasks/update_stock_masters_market_price_and_value.py
+++ backend/stock/stk_master/tasks/update_stock_masters_market_price_and_value.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
+from base.utils import get_admin_user
+from general.gnl_syslog.utils import write_syslog
+
 from stock.models import StockMaster
 from stock.stk_master.utils import get_stock_last_price
 
@@ -13,10 +16,25 @@ def get_updated_stock_master(stock_master, market_price):
 
 @shared_task
 def update_stock_masters_market_price_and_value():
+    admin_user = get_admin_user()
+    write_syslog(
+        'update_stock_masters_market_price_and_value',
+        'begin',
+        admin_user
+    )
+
     stock_masters = StockMaster.objects.filter(enable=True)
     stock_codes = stock_masters.distinct('stock_code')\
         .values_list('stock_code', flat=True)
-    for stock_code in stock_codes:
+    stock_codes_len = len(stock_codes)
+
+    write_syslog(
+        'update_stock_masters_market_price_and_value',
+        f'unique stock code count {stock_codes_len}',
+        admin_user
+    )
+
+    for idx, stock_code in enumerate(stock_codes):
         last_price = get_stock_last_price(stock_code)
         related_stock_masters = StockMaster.objects\
             .filter(stock_code=stock_code, enable=True)
@@ -29,3 +47,14 @@ def update_stock_masters_market_price_and_value():
         related_stock_masters.bulk_update(updated_stock_masters,
                                           ['market_price', 'market_value']
                                           )
+        write_syslog(
+            'update_stock_masters_market_price_and_value',
+            f'{idx}/{stock_codes_len}: {stock_code} {last_price}',
+            admin_user
+        )
+
+    write_syslog(
+        'update_stock_masters_market_price_and_value',
+        'finished',
+        admin_user
+    )
