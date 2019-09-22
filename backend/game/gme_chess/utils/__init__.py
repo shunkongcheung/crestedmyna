9cd3c2f55836de83b6cb2acc285fc25af681cfd3
shun.cheung <shun.cheung@radicasys.com>
2019-09-22 21:23:28 +0800

    backend: rm chess/get_winner

--- backend/game/gme_chess/utils/__init__.py
+++ backend/game/gme_chess/utils/__init__.py
@@ -1,5 +1,5 @@
 from .get_board_from_hash import get_board_from_hash
+from .get_board_winner_and_score import get_board_winner_and_score
 from .get_hash_from_board import get_hash_from_board
 from .get_initial_board import get_initial_board
 from .get_next_boards import get_next_boards
-from .get_winner import get_winner
