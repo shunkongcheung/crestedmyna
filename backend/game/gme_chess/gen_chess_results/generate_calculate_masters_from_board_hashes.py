3cf8249984fef613fd6204d60cc50bb081d589e3
shun.cheung <shun.cheung@radicasys.com>
2019-09-26 19:48:27 +0800

    backend: generate_calculate_masters_from_board_hashes add limit

--- backend/game/gme_chess/gen_chess_results/generate_calculate_masters_from_board_hashes.py
+++ backend/game/gme_chess/gen_chess_results/generate_calculate_masters_from_board_hashes.py
@@ -4,34 +4,28 @@ from general.gnl_lookup.utils import get_lookup_value
 
 from game.models import ChessBoardCalculateMaster
 from game.gme_chess.tasks import create_calculate_children_masters
-from game.gme_chess.utils import (
-    get_board_from_hash,
-    get_board_winner_and_score,
-)
-from game.gme_chess.utils.prefixes import CHS_EMPTY
 
 from .get_created_calculate_master import get_created_calculate_master
 
 
 def generate_calculate_masters_from_board_hashes(board_hashes):
-    total = len(board_hashes)
+    total, MAX_LIMIT = len(board_hashes), 5
     write_debug(f'starting {total}')
     top_level = get_lookup_value('CHESS_CALCULATE_LEVEL')
 
+    created_count = 0
     for idx, board_hash in enumerate(board_hashes):
-        board = get_board_from_hash(board_hash)
-        winner, _ = get_board_winner_and_score(board)
-        if winner != CHS_EMPTY:
-            write_debug(f'[{idx}/{total}] had a winner {winner} {board_hash}')
-            print()
-            continue
-
         create_msg, calculate_master =\
             get_created_calculate_master(board_hash, top_level)
         write_debug(f'[{idx}/{total}]: {create_msg} {board_hash}')
 
         if not calculate_master is None:
             perform_calculation(calculate_master)
+            created_count += 1
+
+        if created_count >= MAX_LIMIT:
+            write_debug(f'[{idx}/{total}]: created{created_count}. exit')
+            return
 
         print()
 
