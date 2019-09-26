67b786abc56836b3071b55da85098d4e009e960d
shun.cheung <shun.cheung@radicasys.com>
2019-09-26 19:50:16 +0800

    backend: gme_sudoku init

--- backend/game/admin.py
+++ backend/game/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 
 # Register your models here.
 from .gme_chess.admin import *
+from .gme_sudoku.admin import *
--- backend/game/models.py
+++ backend/game/models.py
@@ -2,3 +2,4 @@ from django.db import models
 
 # Create your models here.
 from .gme_chess.models import *
+from .gme_sudoku.models import *
