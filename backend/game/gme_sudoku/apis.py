b754ee1aea328b5b7b057679d50f88e0c832c6d8
shun.cheung <shun.cheung@radicasys.com>
2019-09-27 23:17:05 +0800

    backend: SudokuGameRecordMasterObjectAPIView - add defaults

--- backend/game/gme_sudoku/apis.py
+++ backend/game/gme_sudoku/apis.py
@@ -7,7 +7,12 @@ from base.apis import (
 from .models import SudokuGameRecordMaster
 from .serializers import SudokuInitialBoardSerializer
 
-fields = ['start_board', 'current_board', 'used_second', 'is_finished', ]
+fields = ['start_board',
+          'current_board',
+          'solution_board',
+          'used_second',
+          'difficulty',
+          ]
 
 
 class SudokuInitialBoardSerializerAPIView(MyCreateAPIView):
@@ -23,6 +28,12 @@ class SudokuGameRecordMasterObjectAPIView(MyObjectAPIView):
     model = SudokuGameRecordMaster
 
     def get_object(self):
+        defaults = {
+            'start_board': '',
+            'solution_board': '',
+            'current_board': '',
+            'difficulty': 'easy',
+        }
         object, created = self.model.objects\
-            .get_or_create(created_by=self.request.user, enable=True)
+            .get_or_create(created_by=self.request.user, enable=True, defaults=defaults)
         return object
