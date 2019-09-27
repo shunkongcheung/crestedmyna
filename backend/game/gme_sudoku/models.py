52d3b5bc0063899d1dfb55a2df37a74505c1c9e9
shun.cheung <shun.cheung@radicasys.com>
2019-09-27 23:13:25 +0800

    backend: SudokuGameRecordMaster migrations add difficulty

--- backend/game/gme_sudoku/models.py
+++ backend/game/gme_sudoku/models.py
@@ -9,3 +9,11 @@ class SudokuGameRecordMaster(MyBaseModel):
     solution_board = models.CharField(max_length=pow(9, 2))
     current_board = models.CharField(max_length=pow(9, 2))
     used_second = models.IntegerField(default=0)
+    difficulty = models.CharField(
+        max_length=16,
+        choices=[
+            ('easy', 'Easy'),
+            ('medium', 'Medium'),
+            ('difficult', 'Difficult'),
+        ]
+    )
