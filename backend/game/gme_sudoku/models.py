84e77ace5600da2d46d065355440724b0490bbdd
shun.cheung <shun.cheung@radicasys.com>
2019-09-27 23:10:01 +0800

    backend: SudokuGameRecordMaster migrations add solution_board

--- backend/game/gme_sudoku/models.py
+++ backend/game/gme_sudoku/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 class SudokuGameRecordMaster(MyBaseModel):
     start_board = models.CharField(max_length=pow(9, 2))
+    solution_board = models.CharField(max_length=pow(9, 2))
     current_board = models.CharField(max_length=pow(9, 2))
     used_second = models.IntegerField(default=0)
     is_finished = models.BooleanField(default=False)
