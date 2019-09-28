08bba8e09b3fd82406d2b80b93c1d3ae6a84c2aa
shun.cheung <shun.cheung@radicasys.com>
2019-09-28 23:18:23 +0800

    Frontend: Fetches include setErrorState

--- frontend/src/Base/Fetches/index.tsx
+++ frontend/src/Base/Fetches/index.tsx
@@ -1,5 +1,6 @@
 export { default as useDeleteState } from "./useDeleteState";
 export { default as useDetailState } from "./useDetailState";
 export { default as useEditState } from "./useEditState";
+export { default as useErrorState } from "./useErrorState";
 export { default as useFetchFormDataState } from "./useFetchFormDataState";
 export { default as useListState } from "./useListState";
