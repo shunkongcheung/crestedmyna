14bdca884e4c29082d3d3c5100f843a101e15b91
shun.cheung <shun.cheung@radicasys.com>
2019-09-29 10:42:08 +0800

    backend: gnl_media/utils import get_media_file_full_path

--- backend/general/gnl_media/utils/__init__.py
+++ backend/general/gnl_media/utils/__init__.py
@@ -1,3 +1,4 @@
+from .get_media_file_full_path import get_media_file_full_path
 from .get_media_file_name import get_media_file_name
 from .get_media_file_type import get_media_file_type
 from .store_media_file import store_media_file
