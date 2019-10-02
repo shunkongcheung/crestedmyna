6994b86e3172608293a6c6b9e3140f8134d1c27c
shun.cheung <shun.cheung@radicasys.com>
2019-10-02 14:33:44 +0800

    backend: get_schedule_tasks add periodic_create_shareholding_disclosure_records

--- backend/base/management/commands/utils/get_schedule_tasks.py
+++ backend/base/management/commands/utils/get_schedule_tasks.py
@@ -8,6 +8,11 @@ def get_schedule_tasks():
     '''
     return [
         {
+            'name': 'stk_ccass-periodic_create_shareholding_disclosure_records',
+            'task': 'stock.stk_master.tasks.periodic_create_shareholding_disclosure_records.periodic_create_shareholding_disclosure_records',
+            'crontab': '1 1 * * *',
+        },
+        {
             'name': 'stk_master-update_stock_price',
             'task': 'stock.stk_master.tasks.update_stock_masters_market_price_and_value.update_stock_masters_market_price_and_value',
             'crontab': '1 0 * * *',
