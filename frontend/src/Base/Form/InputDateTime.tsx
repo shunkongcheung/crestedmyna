ad148b50a622884698fd157296c7f5a6db2695a4
shun.cheung <shun.cheung@radicasys.com>
2019-10-06 01:06:03 +0800

    frontend: InputDateTime handleBlur

--- frontend/src/Base/Form/InputDateTime.tsx
+++ frontend/src/Base/Form/InputDateTime.tsx
@@ -1,31 +1,44 @@
 import React, { memo, useCallback, useMemo } from "react";
 import { DatePicker, Tooltip } from "antd";
 import { FormikProps } from "formik";
+import moment, { Moment } from "moment";
 import PropTypes from "prop-types";
 
 import useFormInputsState from "./useFormInputsState";
 
 interface IInputDateTimeProps {
+  disabledDate?: (t: Moment | undefined) => boolean;
   label: string;
   mode?: "date" | "datetime";
   name: string;
 }
 
 function InputDateTime({
+  disabledDate,
   label,
   mode = "datetime",
   name,
   ...formikProps
 }: IInputDateTimeProps & FormikProps<{ [x: string]: any }>) {
-  const { inputError, style } = useFormInputsState(name, formikProps);
+  const { inputError, inputValue, style } = useFormInputsState(
+    name,
+    formikProps
+  );
   const { setFieldTouched, setFieldValue } = formikProps;
 
   const handleChange = useCallback(
     value => {
-      setFieldValue(name, value);
+      setFieldValue(name, value && value.isValid() ? value : undefined);
       setFieldTouched(name, true, true);
     },
     [name, setFieldValue, setFieldTouched]
+  );
+  const handleBlur = useCallback(
+    open => {
+      console.log("hey open changed...", name, open);
+      setFieldTouched(name, true, true);
+    },
+    [name, setFieldTouched]
   );
 
   const format = useMemo(
@@ -45,10 +58,14 @@ function InputDateTime({
   return (
     <Tooltip title={inputError}>
       <DatePicker
-        onChange={handleChange}
-        style={{ ...style, width: "100%" }}
-        showTime={mode === "datetime"}
+        disabledDate={disabledDate}
         format={format}
+        placeholder={label}
+        onChange={handleChange}
+        onOpenChange={handleBlur}
+        showTime={mode === "datetime"}
+        style={{ ...style, width: "100%" }}
+        value={inputValue ? moment(inputValue) : undefined}
       />
     </Tooltip>
   );
