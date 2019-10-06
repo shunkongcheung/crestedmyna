8bff4431f25abfec72d986c7bd4d3f0b1827a7ee
shun.cheung <shun.cheung@radicasys.com>
2019-10-06 21:07:35 +0800

    backend: stk_master/update_stock_masters_market_price_and_value update more fields

--- backend/stock/stk_master/tasks/update_stock_masters_market_price_and_value.py
+++ backend/stock/stk_master/tasks/update_stock_masters_market_price_and_value.py
@@ -10,8 +10,13 @@ from stock.stk_master.utils import get_stock_last_status
 
 def get_updated_stock_master(stock_master, market_price, turnover):
     stock_master.market_price = market_price
-    stock_master.market_value = stock_master.share_count * market_price
+
+    market_value = stock_master.share_count * market_price
+    stock_master.market_value = market_value
+
+    stock_master.unrealized_value = market_value - stock_master.unrealized_cost
     stock_master.turnover = turnover
+
     return stock_master
 
 
@@ -35,7 +40,11 @@ def update_stock_masters_market_price_and_value():
             for stock_master in related_stock_masters
         ]
 
-        updated_fields = ['market_price', 'market_value', 'turnover', ]
+        updated_fields = ['market_price',
+                          'market_value',
+                          'turnover',
+                          'unrealized_value',
+                          ]
         related_stock_masters.bulk_update(updated_stock_masters,
                                           updated_fields
                                           )
