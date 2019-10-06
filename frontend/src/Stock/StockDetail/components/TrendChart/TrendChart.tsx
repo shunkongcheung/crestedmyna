8db2af4b789f587bd73c5f8a6af0cd99a4788173
shun.cheung <shun.cheung@radicasys.com>
2019-10-06 20:05:12 +0800

    frontend: TrendChart title and chartType

--- frontend/src/Stock/StockDetail/components/TrendChart/TrendChart.tsx
+++ frontend/src/Stock/StockDetail/components/TrendChart/TrendChart.tsx
@@ -1,5 +1,5 @@
-import React, { memo, useMemo } from "react";
-import { Line } from "react-chartjs-2";
+import React, { createElement, memo, useMemo } from "react";
+import * as Charts from "react-chartjs-2";
 import { Spin } from "antd";
 import { Chart, ChartDataSets } from "chart.js";
 import PropTypes from "prop-types";
@@ -7,16 +7,20 @@ import PropTypes from "prop-types";
 import classNames from "./TrendChart.module.scss";
 
 interface ITrendChartProps {
+  chartType?: "Bar" | "Line";
   isLoading: boolean;
   labels: Array<string>;
   datasets: Array<ChartDataSets>;
+  title: string;
   yAxesUserCallback?: (item: any) => any;
 }
 
 function TrendChart({
+  chartType = "Line",
   datasets,
   isLoading,
   labels,
+	title,
   yAxesUserCallback
 }: ITrendChartProps) {
   const renderedLoading = useMemo(
@@ -30,15 +34,19 @@ function TrendChart({
   const renderedChart = useMemo(
     () => {
       const options: Chart.ChartOptions = {
-        legend: { position: "bottom", display: false }
+        legend: { position: "bottom", display: false },
+        title: { position: "left", display: true, text: title }
       };
       if (yAxesUserCallback)
         options.scales = {
           yAxes: [{ ticks: { callback: yAxesUserCallback } }]
         };
-      return <Line data={{ labels, datasets }} height={70} options={options} />;
+      const ChartElement = Charts[chartType];
+      const data = { labels, datasets };
+      const height = 70;
+      return createElement(ChartElement, { data, options, height });
     },
-    [labels, datasets, yAxesUserCallback]
+    [chartType, labels, datasets, title, yAxesUserCallback]
   );
 
   const renderedContent = useMemo(
@@ -49,9 +57,11 @@ function TrendChart({
 }
 
 TrendChart.propTypes = {
+  chartType: PropTypes.oneOf(["bar", "line"]).isRequired,
   isLoading: PropTypes.bool.isRequired,
   labels: PropTypes.arrayOf(PropTypes.string).isRequired,
   datasets: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
   yAxesUserCallback: PropTypes.func
 };
 export default memo(TrendChart);
