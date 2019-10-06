860c159a9a73f287b38d40735b9ad517b0b900f3
shun.cheung <shun.cheung@radicasys.com>
2019-10-06 20:11:50 +0800

    backend: StockMaster - migrations - turnover and unrealized_cost

--- backend/stock/stk_master/models.py
+++ backend/stock/stk_master/models.py
@@ -5,9 +5,6 @@ from django.db import models
 
 
 class StockMaster(MyBaseModel):
-    stock_code = models.CharField(max_length=64)
-
-    share_count = models.IntegerField(default=0)
     market_price = models.FloatField(default=0)  # update on refresh
     market_value = models.FloatField(default=0.0)  # share_count * market_price
     realized_value = models.FloatField(default=0.0)
@@ -16,5 +13,9 @@ class StockMaster(MyBaseModel):
                                on_delete=models.CASCADE,
                                related_name='stock_masters',
                                )
-
+    stock_code = models.CharField(max_length=64)
+    share_count = models.IntegerField(default=0)
+    turnover = models.FloatField(default=0.0)
+    unrealized_cost = models.FloatField(default=0.0)
+    # (market_price - unrealized_cost) / unrealized_cost
     unrealized_value = models.FloatField(default=0.0)
