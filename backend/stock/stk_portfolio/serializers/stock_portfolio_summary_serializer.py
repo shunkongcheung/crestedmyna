2ec4bbe664e09ab8ff5ddc106223792a1e6079e6
shun.cheung <shun.cheung@radicasys.com>
2019-10-08 21:41:56 +0800

    backend: get_portfolio_summary filter sector(s)

--- backend/stock/stk_portfolio/serializers/stock_portfolio_summary_serializer.py
+++ backend/stock/stk_portfolio/serializers/stock_portfolio_summary_serializer.py
@@ -1,7 +1,8 @@
 from rest_framework.serializers import (
     Serializer,
     FloatField,
-    PrimaryKeyRelatedField,
+    IntegerField,
+    ListField,
 )
 
 from stock.models import StockSectorMaster
@@ -9,20 +10,20 @@ from stock.stk_portfolio.utils import get_portfolio_summary
 
 
 class StockPortfolioSummarySerializer(Serializer):
-    stock_sector = PrimaryKeyRelatedField(
-        required=False,
-        queryset=StockSectorMaster.objects.filter(enable=True),
-    )
+    sectors = ListField(child=IntegerField(), write_only=True, required=False)
 
     market_value = FloatField(read_only=True)
     realized_value = FloatField(read_only=True)
-    share_count = FloatField(read_only=True)
     unrealized_value = FloatField(read_only=True)
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
         return super().__init__(*args, **kwargs)
 
+    def validate_sectors(self, sectors):
+        return StockSectorMaster.objects\
+            .filter(enable=True, id__in=sectors)
+
     def validate(self, data):
-        stock_sector = data.get('stock_sector')
-        return get_portfolio_summary(self.user, stock_sector)
+        stock_sectors = data.get('sectors')
+        return get_portfolio_summary(self.user, stock_sectors)
--- backend/stock/stk_portfolio/utils/get_portfolio_summary.py
+++ backend/stock/stk_portfolio/utils/get_portfolio_summary.py
@@ -2,18 +2,16 @@ from django.db.models import Sum
 from stock.models import StockMaster
 
 
-def get_portfolio_summary(user, sector=None):
-    stock_masters = get_stock_masters(user, sector)
+def get_portfolio_summary(user, sectors):
+    stock_masters = get_stock_masters(user, sectors)
 
     market_value = get_market_value(stock_masters)
     realized_value = get_realized_value(stock_masters)
-    share_count = get_share_count(stock_masters)
     unrealized_value = get_unrealized_value(stock_masters)
 
     return {
         'market_value': market_value,
         'realized_value': realized_value,
-        'share_count': share_count,
         'unrealized_value': unrealized_value,
     }
 
@@ -30,22 +28,16 @@ def get_realized_value(stock_masters):
         .aggregate(sum=Sum('realized_value'))['sum']
 
 
-def get_share_count(stock_masters):
-    return stock_masters\
-        .values('share_count')\
-        .aggregate(sum=Sum('share_count'))['sum']
-
-
 def get_unrealized_value(stock_masters):
     return stock_masters\
         .values('unrealized_value')\
         .aggregate(sum=Sum('unrealized_value'))['sum']
 
 
-def get_stock_masters(user, sector):
+def get_stock_masters(user, sectors):
     stock_masters = StockMaster.objects\
         .filter(enable=True, created_by=user)
-    if not sector is None:
-        stock_masters = stock_masters.filter(sector=sector)
+    if sectors:
+        stock_masters = stock_masters.filter(sector__in=sectors)
 
     return stock_masters
