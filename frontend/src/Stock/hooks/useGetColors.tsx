eb469b83f445dded34b1b923000cd025cb214259
shun.cheung <shun.cheung@radicasys.com>
2019-10-09 17:44:27 +0800

    frontend: useGetColors add colors

--- frontend/src/Stock/hooks/useGetColors.tsx
+++ frontend/src/Stock/hooks/useGetColors.tsx
@@ -3,6 +3,18 @@ import { useCallback } from "react";
 function useGetColors() {
   const getColors = useCallback(() => {
     return [
+      "rgba(54, 162, 235, 0.2)",
+      "rgba(255, 206, 86, 0.2)",
+      "rgba(75, 192, 192, 0.2)",
+      "rgba(153, 102, 255, 0.2)",
+      "rgba(255, 159, 64, 0.2)",
+      "rgba(255, 99, 132, 0.2)",
+      "rgba(255, 159, 64, 0.2)",
+      "rgba(255, 205, 86, 0.2)",
+      "rgba(75, 192, 192, 0.2)",
+      "rgba(54, 162, 235, 0.2)",
+      "rgba(153, 102, 255, 0.2)",
+      "rgba(201, 203, 207, 0.2)",
       "#FF6633",
       "#FFB399",
       "#FF33FF",
