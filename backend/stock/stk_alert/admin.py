45988ca8a59b932aeacbc2e8f76429e2ebfa702b
shun.cheung <shun.cheung@radicasys.com>
2019-10-10 21:41:16 +0800

    backend: StockAlertMaster migrations

--- backend/stock/stk_alert/admin.py
+++ backend/stock/stk_alert/admin.py
@@ -1,14 +1,21 @@
 from base.admin import MyBaseAdmin
 from django.contrib import admin
 
-# from .models import ModelName
+from .models import StockAlertMaster
 
 
-# # Register your models here.
-# class ModelNameAdmin(MyBaseAdmin):
-#     list_display = ['catagory', 'is_public', 'lookup_value', ]
-#     list_filter = []
-#     search_fields = []
+# Register your models here.
+class StockAlertMasterAdmin(MyBaseAdmin):
+    list_display = ['stock_code',
+                    'market_price_value',
+                    'market_price_condition',
+                    'ccass_percent_value',
+                    'ccass_percent_condition',
+                    ]
+    list_filter = ['market_price_condition',
+                   'ccass_percent_condition',
+                   ]
+    search_fields = ['stock_code', ]
 
 
-# admin.site.register(ModelName, ModelNameAdmin)
+admin.site.register(StockAlertMaster, StockAlertMasterAdmin)
--- backend/stock/stk_alert/models.py
+++ backend/stock/stk_alert/models.py
@@ -4,5 +4,26 @@ from django.db import models
 # Create your models here.
 
 
-# class ModelName(MyBaseModel):
-#     pass
+class StockAlertMaster(MyBaseModel):
+    COND_ABOVE = 'COND_ABOVE'
+    COND_BELOW = 'COND_BELOW'
+    COND_CHOICES = [
+        (COND_ABOVE, 'When value is above'),
+        (COND_BELOW, 'When value is below'),
+    ]
+
+    stock_code = models.CharField(max_length=16)
+
+    market_price_value = models.FloatField(default=0)
+    market_price_condition = models.CharField(
+        max_length=16,
+        choices=COND_CHOICES,
+        default=COND_ABOVE,
+    )
+
+    ccass_percent_value = models.FloatField(default=0)
+    ccass_percent_condition = models.CharField(
+        max_length=16,
+        choices=COND_CHOICES,
+        default=COND_ABOVE,
+    )
