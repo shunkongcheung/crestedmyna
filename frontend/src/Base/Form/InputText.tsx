993c85ccee57d0d5c6238975ebcf7e9f5f63649c
shun.cheung <shun.cheung@radicasys.com>
2019-10-11 23:41:14 +0800

    frontend: InputText addons and type

--- frontend/src/Base/Form/InputText.tsx
+++ frontend/src/Base/Form/InputText.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useMemo } from "react";
+import React, { ReactNode, memo, useMemo } from "react";
 import { FormikProps } from "formik";
 import PropTypes from "prop-types";
 import { Input, Tooltip } from "antd";
@@ -6,15 +6,21 @@ import { Input, Tooltip } from "antd";
 import useFormInputsState from "./useFormInputsState";
 
 interface IInputTextProps {
+  addonAfter?: ReactNode;
+  addonBefore?: ReactNode;
   label: string;
   isMask?: boolean;
+  type?: "number";
   name: string;
 }
 
 function InputText({
+  addonAfter,
+  addonBefore,
   label,
   isMask,
   name,
+  type,
   ...formikProps
 }: IInputTextProps & FormikProps<{ [x: string]: any }>) {
   const { inputError, inputValue, style } = useFormInputsState(
@@ -23,31 +29,37 @@ function InputText({
   );
   const { handleBlur, handleChange } = formikProps;
 
-  const type = useMemo(
+  const finalType = useMemo(
     () => {
       if (isMask) return "password";
+      if (type) return type;
     },
-    [isMask]
+    [isMask, type]
   );
 
   return (
     <Tooltip title={inputError}>
       <Input
+        addonAfter={addonAfter}
+        addonBefore={addonBefore}
         name={name}
         placeholder={label}
         onBlur={handleBlur}
         onChange={handleChange}
         style={{ ...style, width: "100%" }}
         value={inputValue || ""}
-        type={type}
+        type={finalType}
       />
     </Tooltip>
   );
 }
 
 InputText.propTypes = {
+  addonAfter: PropTypes.element,
+  addonBefore: PropTypes.element,
   label: PropTypes.string.isRequired,
   isMask: PropTypes.bool,
-  name: PropTypes.string
+  name: PropTypes.string,
+  type: PropTypes.oneOf(["number"])
 };
 export default memo(InputText);
