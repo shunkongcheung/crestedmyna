321b5d199216a333718d819f657a79a8e7ca8bf9
shun.cheung <shun.cheung@radicasys.com>
2019-10-12 11:09:51 +0800

    backend: write_syslog default user

--- backend/general/gnl_syslog/utils/write_syslog.py
+++ backend/general/gnl_syslog/utils/write_syslog.py
@@ -1,13 +1,18 @@
 from django.utils import timezone
+
+from base.utils import get_admin_user
 from general.gnl_syslog.models import SystemLog
 
 
-def write_syslog(name,
-                 message,
-                 created_by,
-                 level=SystemLog.LVL_DEBUG[0],
-                 is_print=False
-                 ):
+def write_syslog(
+    name,
+    message,
+    created_by=None,
+    level=SystemLog.LVL_DEBUG[0],
+    is_print=False
+):
+    if not created_by:
+        created_by = get_admin_user()
 
     if is_print:
         created_by_name = created_by.username
