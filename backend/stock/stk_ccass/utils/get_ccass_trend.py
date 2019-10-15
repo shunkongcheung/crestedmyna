b19fb57b5426c0c063a01f38254d529ad04ebd42
shun.cheung <shun.cheung@radicasys.com>
2019-10-15 09:21:32 +0800

    backend: stk_ccass/get_ccass_trend annotate by stock_code

--- backend/stock/stk_ccass/utils/get_ccass_trend.py
+++ backend/stock/stk_ccass/utils/get_ccass_trend.py
@@ -1,34 +1,46 @@
 from django.db.models import F, Sum, Q
 from datetime import timedelta
 
-from ..models import CCASSParticipantDetail
-
-
-def get_named_ccass_detail():
-    valid_queryset = CCASSParticipantDetail.objects.filter(enable=True)
-    named_queryset = valid_queryset\
-        .annotate(participant_name=F('participant_master__name'))
-
-    return named_queryset
+from stock.models import (
+    CCASSParticipantDetail,
+    StockMaster,
+)
 
 
 def get_annotation(detail_date):
     return Sum('share_percent', filter=Q(detail_date=detail_date))
 
 
+def get_last_day_with_records(cur_date):
+    dates = CCASSParticipantDetail.objects\
+        .filter(enable=True)\
+        .order_by('-detail_date')\
+        .distinct('detail_date')\
+        .values_list('detail_date', flat=True)
+
+    date_list = list(dates)
+    cur_idx = date_list.index(cur_date)
+    if cur_idx == 0:
+        return cur_date
+    return date_list[cur_idx - 1]
+
+
 def get_ccass_trend(date):
-    today, ytd = date, date - timedelta(days=1)
-    named_queryset = get_named_ccass_detail()
-    full_queryset = named_queryset\
-        .values('participant_name')\
+    second_date = date.date()
+    first_date = get_last_day_with_records(second_date)
+
+    full_queryset = CCASSParticipantDetail.objects\
+        .filter(enable=True)\
+        .values('stock_code')\
         .annotate(
-            first_percent=get_annotation(ytd),
-            second_percent=get_annotation(today)
+            first_percent=get_annotation(first_date),
+            second_percent=get_annotation(second_date)
         )\
-        .values('first_percent', 'second_percent', 'participant_name')
+        .values('first_percent', 'second_percent', 'stock_code')
 
     diff_queryset = full_queryset\
         .annotate(diff_percent=F('second_percent')-F('first_percent'))\
         .filter(diff_percent__isnull=False)\
         .order_by('-diff_percent', '-first_percent', )
+
     return diff_queryset
