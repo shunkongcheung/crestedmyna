c79c86b27b3aebcbde000259542e1aa63c99324e
shun.cheung <shun.cheung@radicasys.com>
2019-10-17 09:56:18 +0800

    frontend: stk_news init

--- backend/stock/urls.py
+++ backend/stock/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url, include
 from .stk_alert import urls as stk_alert
 from .stk_ccass import urls as stk_ccass
 from .stk_master import urls as stk_master
+from .stk_news import urls as stk_news
 from .stk_trend import urls as stk_trend
 from .stk_portfolio import urls as stk_profile
 from .stk_sector import urls as stk_sector
@@ -14,6 +15,7 @@ urlpatterns = [
     url(r'^stk_alert/', include(stk_alert)),
     url(r'^stk_ccass/', include(stk_ccass)),
     url(r'^stk_master/', include(stk_master)),
+    url(r'^stk_news/', include(stk_news)),
     url(r'^stk_trend/', include(stk_trend)),
     url(r'^stk_portfolio/', include(stk_profile)),
     url(r'^stk_sector/', include(stk_sector)),
