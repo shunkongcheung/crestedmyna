f768c0316f2c310e865e4101d629e6abf4b6675d
shun.cheung <shun.cheung@radicasys.com>
2019-10-20 09:39:26 +0800

    backend: StockCCASSAndPriceSummaryDetail - migrations unique_together

--- backend/stock/stk_trend/models.py
+++ backend/stock/stk_trend/models.py
@@ -7,3 +7,6 @@ class StockCCASSAndPriceSummaryDetail(MyBaseModel):
     nominal_price = models.FloatField()
     stock_code = models.CharField(max_length=64)
     participant_percent = models.FloatField()
+
+    class Meta:
+        unique_together = ['detail_date', 'stock_code', ]
