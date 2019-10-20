6d74ef8f5ce745a33cca94c41e3139b910120842
shun.cheung <shun.cheung@radicasys.com>
2019-10-20 10:13:25 +0800

    backend: stk_master/get_stock_last_status accept date

--- backend/stock/stk_master/utils/get_stock_last_status.py
+++ backend/stock/stk_master/utils/get_stock_last_status.py
@@ -16,15 +16,19 @@ def get_end_date_str():
     return ytd_str
 
 
-def get_stock_last_status(stock_code):
+def get_stock_last_status(stock_code, date=None):
     quandl = get_quandl()
     start_date, end_date = get_start_date_str(), get_end_date_str()
     stock = f'HKEX/{stock_code}'
     data = quandl.get(stock, start_date=start_date, end_date=end_date)
 
-    last_record = data.tail(1)
-    record_dict = json.loads(last_record.to_json(orient='records'))[0]
+    if date:
+        d_index = data.index.get_loc(date)
+        record_dict = data.iloc[d_index]
+    else:
+        target_record = data.tail(1)
+        record_dict = json.loads(target_record.to_json(orient='records'))[0]
+
     nominal_price = record_dict['Nominal Price']
-    # turnover = record_dict['Turnover (000)']
     turnover = record_dict['Share Volume (000)']
     return nominal_price, turnover
