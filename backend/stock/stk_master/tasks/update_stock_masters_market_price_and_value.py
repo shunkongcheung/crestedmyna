78d5f045288378be732e12f877c1f3a8ef389c1c
shun.cheung <shun.cheung@radicasys.com>
2019-10-23 23:29:54 +0800

    backend: stk_master/update_stock_masters_market_price_and_value filter admin stock_codes

--- backend/stock/stk_master/tasks/update_stock_masters_market_price_and_value.py
+++ backend/stock/stk_master/tasks/update_stock_masters_market_price_and_value.py
@@ -8,24 +8,10 @@ from stock.models import StockMaster
 from stock.stk_master.utils import get_stock_last_status
 
 
-def get_updated_stock_master(stock_master, market_price, turnover):
-    stock_master.market_price = market_price
-
-    market_value = stock_master.share_count * market_price
-    stock_master.market_value = market_value
-
-    stock_master.unrealized_value = market_value - stock_master.unrealized_cost
-    stock_master.turnover = turnover
-
-    return stock_master
-
-
 @shared_task
 def update_stock_masters_market_price_and_value():
     w_log('begin')
-    stock_masters = StockMaster.objects.filter(enable=True)
-    stock_codes = stock_masters.distinct('stock_code')\
-        .values_list('stock_code', flat=True)
+    stock_codes = get_stock_codes()
     stock_codes_len = len(stock_codes)
 
     w_log(f'unique stock code count {stock_codes_len}')
@@ -51,10 +37,27 @@ def update_stock_masters_market_price_and_value():
     w_log('finished')
 
 
-def w_log(message):
+def get_stock_codes():
     admin_user = get_admin_user()
-    write_syslog(
-        'update_stock_masters_market_price_and_value',
-        message,
-        admin_user
-    )
+    stock_masters = StockMaster.objects.filter(enable=True)
+    stock_codes = stock_masters\
+        .exclude(created_by=admin_user)\
+        .distinct('stock_code')\
+        .values_list('stock_code', flat=True)
+    return stock_codes
+
+
+def get_updated_stock_master(stock_master, market_price, turnover):
+    stock_master.market_price = market_price
+
+    market_value = stock_master.share_count * market_price
+    stock_master.market_value = market_value
+
+    stock_master.unrealized_value = market_value - stock_master.unrealized_cost
+    stock_master.turnover = turnover
+
+    return stock_master
+
+
+def w_log(message):
+    write_syslog('update_stock_masters_market_price_and_value', message)
