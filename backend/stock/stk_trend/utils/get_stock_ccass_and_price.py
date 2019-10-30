479ff06f567c8a88e85893fe15dabe91334ec18a
shun.cheung <shun.cheung@radicasys.com>
2019-10-30 09:17:27 +0800

    backend: stk_trend/get_stock_ccass_and_price lxml parsing get correct value

--- backend/stock/stk_trend/utils/get_stock_ccass_and_price.py
+++ backend/stock/stk_trend/utils/get_stock_ccass_and_price.py
@@ -10,7 +10,15 @@ def get_stock_ccass_and_price(stock_code, date):
 
 def get_stock_participant_percent(stock_code, date):
     html_lxml = get_lxml_from_stock_code(stock_code, date)
-    parent_item = html_lxml.xpath('//div[@class="percent-of-participants"]')[1]
-    percent_text = parent_item.xpath('.//div[@class="value"]')[0].text
+
+    total_div_class = '//div[@class="ccass-search-datarow ccass-search-total"]'
+    total_div = html_lxml.xpath(total_div_class)[0]
+
+    parent_div_class = './/div[@class="percent-of-participants"]'
+    parent_item = total_div.xpath(parent_div_class)[0]
+
+    value_div_class = './/div[@class="value"]'
+    percent_text = parent_item.xpath(value_div_class)[0].text
+
     participant_pecent = float(percent_text.replace('%', ''))
     return participant_pecent
