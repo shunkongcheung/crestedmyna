8a56c3efacc50c11dc08cdfb941b44403c56b587
shun.cheung <shun.cheung@radicasys.com>
2019-11-06 15:25:12 +0800

    backend: gnl_media/get_media_file_full_path just add a directory

--- backend/general/gnl_media/utils/get_media_file_full_path.py
+++ backend/general/gnl_media/utils/get_media_file_full_path.py
@@ -1,11 +1,8 @@
 from django.conf import settings
-import os
-
-BASE_DIR = settings.BASE_DIR
 IS_DEVELOPMENT = settings.IS_DEVELOPMENT
 
 
 def get_media_file_full_path(file_name):
     if settings.IS_DEVELOPMENT:
-        return os.path.join(f'{BASE_DIR}', 'base/static/m', file_name)
-    return os.path.join('/usr/local/casualapp/media', file_name)
+        return f'dev/{file_name}'
+    return f'prod/{file_name}'
