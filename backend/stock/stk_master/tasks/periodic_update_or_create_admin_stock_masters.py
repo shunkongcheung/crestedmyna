c2d118337fd411faf5d2d6ecc75e4fa1b1d74294
shun.cheung <shun.cheung@radicasys.com>
2019-11-07 12:35:51 +0800

    backend: periodic_update_or_create_admin_stock_masters use fetch_app_action

--- backend/stock/stk_master/tasks/periodic_update_or_create_admin_stock_masters.py
+++ backend/stock/stk_master/tasks/periodic_update_or_create_admin_stock_masters.py
@@ -1,10 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
-from base.utils import get_admin_user
+from base.utils import fetch_app_action
 from general.gnl_syslog.utils import write_syslog
-from ..models import StockMaster
-from ..serializers import StockMasterSerializer
 from ..utils import get_hkex_stock_codes
 
 
@@ -15,23 +13,12 @@ def periodic_update_or_create_admin_stock_masters():
     code_count = len(stock_codes)
     w_log(f'total {code_count}')
 
-    admin_user = get_admin_user()
-    serializer_class = get_serializer_class()
     for idx, stock_code in enumerate(stock_codes):
-        serializer = serializer_class(data={'stock_code': stock_code, })
-        if serializer.is_valid():
-            serializer.validated_data['created_by'] = admin_user
-            serializer.save()
-        w_log(f'{idx}/{code_count}: submitted {stock_code}')
+        data = {'stock_code': stock_code, }
+        url = 'stock/stk_master/create'
+        fetch_app_action(url, data)
 
-
-def get_serializer_class():
-    class SerializerClass(StockMasterSerializer):
-        class Meta:
-            model = StockMaster
-            fields = ['stock_code', 'created_by', 'created_at', 'modified_at']
-
-    return SerializerClass
+    w_log('finish')
 
 
 def w_log(message):
