9a5ecafc8c86e67c1aefe9553528b2e4afbd9473
shun.cheung <shun.cheung@radicasys.com>
2019-11-08 13:29:31 +0800

    backend: get_schedule_tasks update names

--- backend/base/management/commands/utils/get_schedule_tasks.py
+++ backend/base/management/commands/utils/get_schedule_tasks.py
@@ -19,7 +19,7 @@ def get_schedule_tasks():
         },
         {
             'name': 'stk_master-periodic_update_or_create_admin_stock_master',
-            'task': 'stock.stk_master.tasks.periodic_update_or_create_admin_stock_master.periodic_update_or_create_admin_stock_master',
+            'task': 'stock.stk_master.tasks.periodic_update_or_create_admin_stock_masters.periodic_update_or_create_admin_stock_masters',
             'crontab': '* 9-17 * * 1-5',
         },
         {
