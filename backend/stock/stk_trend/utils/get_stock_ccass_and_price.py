8a61fabc9bbf1d1d47ac9cfde1d85cf8309cf082
shun.cheung <shun.cheung@radicasys.com>
2019-11-08 12:39:53 +0800

    backend: stk_trend/get_stock_ccass_and_price add participant_share

--- backend/stock/stk_trend/utils/get_stock_ccass_and_price.py
+++ backend/stock/stk_trend/utils/get_stock_ccass_and_price.py
@@ -4,13 +4,43 @@ from stock.stk_ccass.utils import get_lxml_from_stock_code
 
 def get_stock_ccass_and_price(stock_code, date):
     nominal_price, turnover = get_stock_last_status(stock_code, date)
-    participant_pecent = get_stock_participant_percent(stock_code, date)
-    return participant_pecent, nominal_price, turnover
 
-
-def get_stock_participant_percent(stock_code, date):
     html_lxml = get_lxml_from_stock_code(stock_code, date)
+    participant_pecent = get_stock_participant_percent(html_lxml)
+    participant_share = get_stock_participant_share(html_lxml)
 
+    return participant_pecent, participant_share, nominal_price, turnover
+
+
+def get_stock_participant_share(html_lxml):
+    total_div_class = '//div[@class="ccass-search-datarow ccass-search-total"]'
+    total_div = html_lxml.xpath(total_div_class)[0]
+
+    parent_div_class = './/div[@class="shareholding"]'
+    parent_item = total_div.xpath(parent_div_class)[0]
+
+    value_div_class = './/div[@class="value"]'
+    percent_text = parent_item.xpath(value_div_class)[0].text
+
+    participant_share = float(percent_text.replace(',', ''))
+    return participant_share
+
+
+def get_stock_participant_percent(html_lxml):
+    total_div_class = '//div[@class="ccass-search-datarow ccass-search-total"]'
+    total_div = html_lxml.xpath(total_div_class)[0]
+
+    parent_div_class = './/div[@class="percent-of-participants"]'
+    parent_item = total_div.xpath(parent_div_class)[0]
+
+    value_div_class = './/div[@class="value"]'
+    percent_text = parent_item.xpath(value_div_class)[0].text
+
+    participant_pecent = float(percent_text.replace('%', ''))
+    return participant_pecent
+
+
+def get_stock_participant_percent(html_lxml):
     total_div_class = '//div[@class="ccass-search-datarow ccass-search-total"]'
     total_div = html_lxml.xpath(total_div_class)[0]
 
