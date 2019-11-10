95529a0e876b482d7737653d949eae26514cf35c
shun.cheung <shun.cheung@radicasys.com>
2019-11-11 00:16:54 +0800

    frontend: StockTxAdd useStockTxAdd and new DateTime

--- frontend/src/Stock/StockDetail/StockTxAdd/StockTxAdd.tsx
+++ frontend/src/Stock/StockDetail/StockTxAdd/StockTxAdd.tsx
@@ -1,11 +1,12 @@
-import React, { memo, useMemo } from "react";
+import React, { memo, useCallback, useMemo } from "react";
 import { Button } from "antd";
-import { withFormik, FormikProps } from "formik";
+import { Formik } from "formik";
 import PropTypes from "prop-types";
-import * as Yup from "yup";
 
-import { InputSelect, InputText, InputDateTime } from "../../../Base/Form";
+import { InputSelect, InputText } from "../../../Base/Form";
+import InputDateTime from "../../../Base/Form/InputDateTime2";
 
+import useStockTxAdd from "./useStockTxAdd";
 import classNames from "./StockTxAdd.module.scss";
 
 interface IStockTx {
@@ -18,76 +19,70 @@ interface IStockTxAddProps {
   handleAddTx: (st: IStockTx, f: any) => any;
 }
 
-function StockTxAdd(formikProps: IStockTxAddProps & FormikProps<IStockTx>) {
-  const { handleSubmit } = formikProps;
-  const renderedTxType = useMemo(
-    () => {
-      const choices = [
-        { name: "BUY", id: "BUY" },
-        { name: "SELL", id: "SELL" },
-        { name: "DIVIDEND", id: "DIVIDEND" }
-      ];
-      return (
-        <InputSelect
-          {...formikProps}
-          choices={choices}
-          label="Transaction type"
-          name="txType"
-        />
-      );
-    },
-    [formikProps]
+function StockTxAdd({ handleAddTx }: IStockTxAddProps) {
+  const { handleSubmit, validationSchmea } = useStockTxAdd(handleAddTx);
+
+  const renderTxType = useCallback(formikProps => {
+    const choices = [
+      { name: "BUY", id: "BUY" },
+      { name: "SELL", id: "SELL" },
+      { name: "DIVIDEND", id: "DIVIDEND" }
+    ];
+    return (
+      <InputSelect
+        {...formikProps}
+        choices={choices}
+        label="Transaction type"
+        name="txType"
+      />
+    );
+  }, []);
+
+  const initialValues = useMemo<IStockTx>(
+    () => ({
+      txType: "BUY",
+      shareCount: 0,
+      price: 0,
+      txAt: new Date()
+    }),
+    []
   );
+
   return (
-    <div className={classNames.container}>
-      <div className={classNames.addCol} style={{ marginLeft: 0 }}>
-        {renderedTxType}
-      </div>
-      <div className={classNames.addCol}>
-        <InputText {...formikProps} label="Share" name="shareCount" />
-      </div>
-      <div className={classNames.addCol}>
-        <InputText {...formikProps} label="Price" name="price" />
-      </div>
-      <div className={classNames.addCol}>
-        <InputDateTime {...formikProps} label="Date" name="txAt" mode="date" />
-      </div>
-      <div className={classNames.iconContainer}>
-        <Button
-          type="primary"
-          shape="circle"
-          icon="plus"
-          onClick={handleSubmit as any}
-        />
-      </div>
-    </div>
+    <Formik
+      validationSchmea={validationSchmea}
+      onSubmit={handleSubmit}
+      initialValues={initialValues}
+    >
+      {formikProps => (
+        <div className={classNames.container}>
+          <div className={classNames.addCol} style={{ marginLeft: 0 }}>
+            {renderTxType(formikProps)}
+          </div>
+          <div className={classNames.addCol}>
+            <InputText label="Share" name="shareCount" />
+          </div>
+          <div className={classNames.addCol}>
+            <InputText label="Price" name="price" />
+          </div>
+          <div className={classNames.addCol}>
+            <InputDateTime label="Date" name="txAt" mode="date" />
+          </div>
+          <div className={classNames.iconContainer}>
+            <Button
+              type="primary"
+              shape="circle"
+              icon="plus"
+              onClick={handleSubmit as any}
+            />
+          </div>
+        </div>
+      )}
+    </Formik>
   );
 }
 
 StockTxAdd.propTypes = {
   handleAddTx: PropTypes.func.isRequired
 };
-export default withFormik<IStockTxAddProps, IStockTx>({
-  validationSchema: Yup.object().shape({
-    txType: Yup.string()
-      .oneOf(["BUY", "SELL", "DIVIDEND"])
-      .required(),
-    txAt: Yup.date().required(),
-    price: Yup.number()
-      .min(0.00000001)
-      .required(),
-    shareCount: Yup.number()
-      .min(0.00000001)
-      .required()
-  }),
-  handleSubmit: async (values, { props: { handleAddTx }, ...formikProps }) => {
-    const submitValues: IStockTx = {
-      txType: values.txType,
-      txAt: values.txAt,
-      price: values.price,
-      shareCount: values.shareCount
-    };
-    await handleAddTx(submitValues, formikProps);
-    formikProps.setSubmitting(false);
-  }
-})(memo(StockTxAdd));
+export default memo(StockTxAdd);
