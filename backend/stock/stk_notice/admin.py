ce55af20041065ecdaec2a9ef22e6b095ead3080
shun.cheung <shun.cheung@radicasys.com>
2019-11-11 20:08:42 +0800

    backend: stk_notice init

---
+++ backend/stock/stk_notice/admin.py
@@ -0,0 +1,14 @@
+from base.admin import MyBaseAdmin
+from django.contrib import admin
+
+# from .models import ModelName
+
+
+# # Register your models here.
+# class ModelNameAdmin(MyBaseAdmin):
+#     list_display = ['catagory', 'is_public', 'lookup_value', ]
+#     list_filter = []
+#     search_fields = []
+
+
+# admin.site.register(ModelName, ModelNameAdmin)
