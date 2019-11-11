822ee9c23a6f8bd76060d9d1d239adafbe3d2099
shun.cheung <shun.cheung@radicasys.com>
2019-11-12 07:38:51 +0800

    backend: update_stock_master_market_realtime_price_and_value

--- backend/stock/stk_master/tasks/__init__.py
+++ backend/stock/stk_master/tasks/__init__.py
@@ -4,6 +4,12 @@ from .periodic_update_or_create_admin_stock_masters import (
 from .periodic_update_stock_masters_market_price_and_value import (
     periodic_update_stock_masters_market_price_and_value,
 )
+from .periodic_update_stock_masters_market_realtime_price_and_value import (
+    periodic_update_stock_masters_market_realtime_price_and_value,
+)
 from .update_stock_master_market_price_and_value import (
     update_stock_master_market_price_and_value,
+)
+from .update_stock_master_market_realtime_price_and_value import (
+    update_stock_master_market_realtime_price_and_value,
 )
