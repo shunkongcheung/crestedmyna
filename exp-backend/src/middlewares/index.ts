7ff8c33f3abe21503389532ceee016e312b6a31a
shun kong cheung <shunkong.cheung@gmail.com>
2020-02-28 18:40:18 -0500

    feat(middlewares & initDb & request.d): removed

--- exp-backend/src/middlewares/index.ts
+++
@@ -1,7 +0,0 @@
-import auth from "./auth";
-import bodyFormatter from "./bodyFormatter";
-import errorHandler from "./errorHandler";
-import logger from "./logger";
-import validateRequest from "./validateRequest";
-
-export { auth, bodyFormatter, errorHandler, logger, validateRequest };
--- exp-backend/src/middlewares/logger.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import getContentDesc from "./getContentDesc";
-
-const logger = (req: Request, res: Response, next: NextFunction) => {
-  console.log(getContentDesc(req));
-  next();
-};
-
-export default logger;
