18957df7db6f2777191d6af3e23f90bf243dfcaf
shun kong cheung <shunkong.cheung@gmail.com>
2020-02-28 18:39:43 -0500

    feat(User): use package BaseUser

--- exp-backend/src/entities/index.ts
+++ exp-backend/src/entities/index.ts
@@ -1,6 +1,5 @@
-import File from "./File";
 import SudokuBoard from "./SudokuBoard";
 import Todo from "./Todo";
 import User from "./User";
 
-export { File, SudokuBoard, Todo, User };
+export { SudokuBoard, Todo, User };
