95c1d5dad04dc59cd9f4d092a09bce3998d787be
shun kong cheung <shunkong.cheung@gmail.com>
2020-02-28 23:02:13 -0500

    feat(base): removed

--- exp-backend/src/base.ts
+++
@@ -1,9 +0,0 @@
-import { getExpressApp } from "shunkongcheung-express-starter";
-
-import router from "./routes";
-import { User } from "./entities";
-
-const expressApp = getExpressApp({ router, userModel: User });
-const { app, serverlessHandler, getController } = expressApp;
-
-export { app, serverlessHandler, getController };
