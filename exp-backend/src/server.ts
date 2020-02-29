56f1fff2eea9079e7b2abbb0f26f75dfe0db1fef
shun kong cheung <shunkong.cheung@gmail.com>
2020-02-28 23:02:32 -0500

    feat(server): import getExpressApp

--- exp-backend/src/server.ts
+++ exp-backend/src/server.ts
@@ -1,4 +1,10 @@
-import { app, serverlessHandler } from "./base";
+import { getExpressApp } from "shunkongcheung-express-starter";
+
+import router from "./routes";
+import { User } from "./entities";
+
+const expressApp = getExpressApp({ router, userModel: User });
+const { app, serverlessHandler } = expressApp;
 
 // finish and export
 export const handler = serverlessHandler;
