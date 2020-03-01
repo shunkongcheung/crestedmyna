97f3c773dd3f6207fa9cf222722b4ab757564e3c
shun kong cheung <shunkong.cheung@gmail.com>
2020-02-29 22:46:46 -0500

    feat(src): clean up unused files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy, useMemo } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { Spin } from "antd";
+
+// const GameContainer = lazy(() => import("./Game"));
+
+function FourOFour() {
+  return <div>404</div>;
+}
+
+function RouteTable() {
+  return (
+    <Switch>
+      <Route path="/game" component={() => <div>hihi</div>} />
+      <Route path="/" component={FourOFour} />
+    </Switch>
+  );
+}
 
 function App() {
+  const renderedFallback = useMemo(
+    () => (
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <Spin />
+      </div>
+    ),
+    []
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={renderedFallback}>
+        <RouteTable />
+      </Suspense>
+    </Router>
   );
 }
 
