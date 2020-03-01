ba5154c84fc9161fb88baec1d1590d00ff0c804f
shun kong cheung <shunkong.cheung@gmail.com>
2020-03-01 01:28:42 -0500

    feat(App): add various providers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
-import React, { Suspense, lazy, useMemo } from "react";
+import React, { Suspense, lazy } from "react";
+import { Spin, notification } from "antd";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AuthContextProvider } from "react-accessories";
+import styled, { ThemeProvider } from "styled-components";
 
-import { Spin } from "antd";
+const Game = lazy(() => import("./Game"));
 
-// const GameContainer = lazy(() => import("./Game"));
+const Fallback = styled.div.attrs({
+  children: <Spin />
+})`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 function FourOFour() {
   return <div>404</div>;
@@ -12,34 +22,49 @@ function FourOFour() {
 function RouteTable() {
   return (
     <Switch>
-      <Route path="/game" component={() => <div>hihi</div>} />
-      <Route path="/" component={FourOFour} />
+      <Route path="/game">
+        <Game />
+      </Route>
+      <Route path="/">
+        <FourOFour />
+      </Route>
     </Switch>
   );
 }
 
-function App() {
-  const renderedFallback = useMemo(
-    () => (
-      <div
-        style={{
-          height: "100vh",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center"
-        }}
-      >
-        <Spin />
-      </div>
-    ),
-    []
-  );
+const notify = (
+  msg: string,
+  lvl: "success" | "error" | "info" | "none" | "warn"
+) => {
+  if (lvl !== "none")
+    notification[lvl]({
+      message: "Something went wrong",
+      description: msg
+    });
+};
+const theme = {
+  colors: {
+    primary: "#e94e80",
+    secondary: "#e94e50",
+    tertiary: "#373a47",
+    quaternary: "#44485c"
+  },
+  size: {
+    medium: "759px",
+    small: "47px"
+  }
+};
 
+function App() {
   return (
     <Router>
-      <Suspense fallback={renderedFallback}>
-        <RouteTable />
-      </Suspense>
+      <ThemeProvider theme={theme}>
+        <AuthContextProvider defaultUser={{ username: "" }} notify={notify}>
+          <Suspense fallback={<Fallback />}>
+            <RouteTable />
+          </Suspense>
+        </AuthContextProvider>
+      </ThemeProvider>
     </Router>
   );
 }
