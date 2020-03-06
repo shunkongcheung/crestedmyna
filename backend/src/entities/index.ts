5913f93fadbb82d772517e14392995eb3db6a13a
shun kong cheung <shunkong.cheung@gmail.com>
2020-03-05 19:55:14 -0500

    feat(entities): export ComicPage

--- backend/src/entities/index.ts
+++ backend/src/entities/index.ts
@@ -1,7 +1,8 @@
 import ComicChapter from "./ComicChapter";
 import ComicMaster from "./ComicMaster";
+import ComicPage from "./ComicPage";
 import File from "./File";
 import SudokuBoard from "./SudokuBoard";
 import User from "./User";
 
-export { ComicChapter, ComicMaster, File, SudokuBoard, User };
+export { ComicChapter, ComicMaster, ComicPage, File, SudokuBoard, User };
