8a72e255ae4e4e1d2872ee4b38dce19399423168
shun kong cheung <shunkong.cheung@gmail.com>
2020-03-05 22:46:19 -0500

    feat(getArgFromHtml): init

--- backend/src/routes/tasks/genComicPages/getArgFromHtml.ts
+++ backend/src/routes/tasks/genComicPages/getArgFromHtml.ts
@@ -1,5 +1,12 @@
 function getArgFromHtml(htmlBody: string) {
-  console.log(htmlBody);
+  const args = htmlBody.split("p}(")[1].split(",{}))")[0];
+  const firstArg = `${args.split(`';'`)[0]}';'`;
+  const restArgs = args.split(`';'`)[1].split(",");
+  const secondArg = Number(restArgs[1]);
+  const thirdArg = Number(restArgs[2]);
+  const forthArg = restArgs[3].split(`'.split('|')`)[0].split("|");
+  const fifthArg = Number(restArgs[4]);
+  return [firstArg, secondArg, thirdArg, forthArg, fifthArg, {}];
 }
 
 export default getArgFromHtml;
