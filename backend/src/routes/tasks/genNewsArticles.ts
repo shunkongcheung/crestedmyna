2fb612adcdeeac5256eb2ac74af6aa3d675ec060
shun kong cheung <shunkong.cheung@gmail.com>
2020-03-06 21:00:16 -0500

    feat(genNewsArtcles): init

---
+++ backend/src/routes/tasks/genNewsArticles.ts
@@ -0,0 +1,62 @@
+import { body } from "express-validator";
+import moment from "moment";
+import nodeFetch from "node-fetch";
+import { getConnection } from "typeorm";
+import uuid from "uuid";
+
+import isAdminMiddleware from "./isAdminMiddleware";
+import { NewsArticle } from "../../entities";
+import getController from "../../getController";
+
+interface ArticleFromApi {
+  source: {
+    id?: string;
+    name?: string;
+  };
+  author?: string;
+  title?: string;
+  description?: string;
+  url?: string;
+  urlToImage?: string;
+  publishedAt?: string;
+}
+
+const validations = [isAdminMiddleware];
+
+async function fetchNewsFromApi() {
+  const res = await nodeFetch(
+    `http://newsapi.org/v2/top-headlines?country=hk&apiKey=${process.env.NEWS_API_KEY}`
+  );
+  const { articles } = await res.json();
+  return articles.map(article => ({
+    sourceId: article.id || "",
+    sourceName: article.name || "",
+    author: article.author || "",
+    title: article.title || "",
+    description: article.description || "",
+    url: article.url || uuid.v4(),
+    urlToImage: article.urlToImage || "",
+    publishedAt: moment(article.publishedAt)
+  }));
+}
+
+async function genNewsArticles() {
+  const results = await fetchNewsFromApi();
+  getConnection()
+    .createQueryBuilder()
+    .insert()
+    .into(NewsArticle)
+    .values(results)
+    .onConflict(`("url") DO NOTHING`)
+    .execute();
+  return [null, results];
+}
+
+const controller = getController({
+  model: NewsArticle,
+  allowedMethods: ["create"],
+  validations: { create: validations } as any,
+  transformCreateData: genNewsArticles
+});
+
+export default controller;
--- backend/src/routes/tasks/index.ts
+++ backend/src/routes/tasks/index.ts
@@ -3,11 +3,13 @@ import express from "express";
 import genComicChapters from "./genComicChapters";
 import genComicMasters from "./genComicMasters";
 import genComicPages from "./genComicPages";
+import genNewsArticles from "./genNewsArticles";
 
 const router = express.Router();
 
 router.use("/gen-comic-chapters", genComicChapters);
 router.use("/gen-comic-masters", genComicMasters);
 router.use("/gen-comic-pages", genComicPages);
+router.use("/gen-news-articles", genNewsArticles);
 
 export default router;
